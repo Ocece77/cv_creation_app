466bce44d1fe921a14a92bbd7bd48f659ea3b198
Ocece77 <oceanekasindupro@gmail.com>
2024-05-23 03:17:43 +0200

    ghpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './home/Home';
 import Generator from './generator/Generator';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/generator" element={<Generator />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
